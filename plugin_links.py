41b56c34dcc7428c0b66a00904d3b4a56235d927
ChainickSupra <150857998+ChainickSupra@users.noreply.github.com>
2024-12-20 16:38:21 +0500

    Add files via upload

---
+++ plugin_links.py
@@ -0,0 +1,176 @@
+plugin_links = {
+    # Концепция
+    "Инсоляция": {
+        "video": "https://vkvideo.ru/playlist/-222525145_2/video-222525145_456239061",
+        "pdf": "https://drive.google.com/file/d/1xi55LWtSPFSahXdpAghvDSy5JK0CPley/view?usp=sharing"
+    },
+    "КЕО": {
+        "video": "https://vkvideo.ru/playlist/-222525145_2/video-222525145_456239126",
+        "pdf": "https://drive.google.com/file/d/1xi55LWtSPFSahXdpAghvDSy5JK0CPley/view?usp=sharing"
+    },
+    "Генерация парков": {
+        "video": "https://vkvideo.ru/playlist/-222525145_2/video-222525145_456239018",
+        "pdf": "https://drive.google.com/file/d/1xi55LWtSPFSahXdpAghvDSy5JK0CPley/view?usp=sharing"
+    },
+    "Генерация деревьев": {
+        "video": "https://vkvideo.ru/playlist/-222525145_2/video-222525145_456239023",
+        "pdf": "https://drive.google.com/file/d/1xi55LWtSPFSahXdpAghvDSy5JK0CPley/view?usp=sharing"
+    },
+    "Разлиновка модели": {
+        "video": "https://vkvideo.ru/playlist/-222525145_2/video-222525145_456239076",
+        "pdf": "https://drive.google.com/file/d/1xi55LWtSPFSahXdpAghvDSy5JK0CPley/view?usp=sharing"
+    },
+    "3D сетки": {
+        "video": "https://vkvideo.ru/playlist/-222525145_2/video-222525145_456239026",
+        "pdf": "https://drive.google.com/file/d/1xi55LWtSPFSahXdpAghvDSy5JK0CPley/view?usp=sharing"
+    },
+    "БыстроТЭПы": {
+        "video": "https://vkvideo.ru/playlist/-222525145_2/video-222525145_456239065",
+        "pdf": "https://drive.google.com/file/d/1xi55LWtSPFSahXdpAghvDSy5JK0CPley/view?usp=sharing"
+    },
+    "Подсчет площадей": {
+        "video": "https://vkvideo.ru/playlist/-222525145_2/video-222525145_456239123",
+        "pdf": "https://drive.google.com/file/d/1xi55LWtSPFSahXdpAghvDSy5JK0CPley/view?usp=sharing"
+    },
+    # Архитектура
+    "Определить помещение": {
+        "video": "https://vkvideo.ru/playlist/-222525145_3/video-222525145_456239110",
+        "pdf": "https://drive.google.com/file/d/1xi55LWtSPFSahXdpAghvDSy5JK0CPley/view?usp=sharing"
+    },
+    "Расчет плинтуса": {
+        "video": "https://vkvideo.ru/playlist/-222525145_3/video-222525145_456239086",
+        "pdf": "https://drive.google.com/file/d/1xi55LWtSPFSahXdpAghvDSy5JK0CPley/view?usp=sharing"
+    },
+    "Отделка": {
+        "video": "https://vkvideo.ru/playlist/-222525145_3/video-222525145_456239063",
+        "pdf": "https://drive.google.com/file/d/1xi55LWtSPFSahXdpAghvDSy5JK0CPley/view?usp=sharing"
+    },
+    "Копировать отделку": {
+        "video": "https://vkvideo.ru/playlist/-222525145_3/video-222525145_456239085",
+        "pdf": "https://drive.google.com/file/d/1xi55LWtSPFSahXdpAghvDSy5JK0CPley/view?usp=sharing"
+    },
+    "Проемы по дверям/окнам из связи": {
+        "video": "https://vkvideo.ru/playlist/-222525145_3/video-222525145_456239075",
+        "pdf": "https://drive.google.com/file/d/1xi55LWtSPFSahXdpAghvDSy5JK0CPley/view?usp=sharing"
+    },
+    "Соединение полов": {
+        "video": "https://vkvideo.ru/playlist/-222525145_3/video-222525145_456239084",
+        "pdf": "https://drive.google.com/file/d/1xi55LWtSPFSahXdpAghvDSy5JK0CPley/view?usp=sharing"
+    },
+    "Планировка": {
+        "video": "https://vkvideo.ru/playlist/-222525145_3/video-222525145_456239089",
+        "pdf": "https://drive.google.com/file/d/1xi55LWtSPFSahXdpAghvDSy5JK0CPley/view?usp=sharing"
+    },
+    "Округление площади": {
+        "video": "https://vkvideo.ru/playlist/-222525145_3/video-222525145_456239092",
+        "pdf": "https://drive.google.com/file/d/1xi55LWtSPFSahXdpAghvDSy5JK0CPley/view?usp=sharing"
+    },
+    "Нумерация квартир": {
+        "video": "https://vkvideo.ru/playlist/-222525145_3/video-222525145_456239087",
+        "pdf": "https://drive.google.com/file/d/1xi55LWtSPFSahXdpAghvDSy5JK0CPley/view?usp=sharing"
+    },
+    # Конструктив
+    "Сборка арматуры": {
+        "video": "https://vkvideo.ru/playlist/-222525145_4/video-222525145_456239097",
+        "pdf": "https://drive.google.com/file/d/1xi55LWtSPFSahXdpAghvDSy5JK0CPley/view?usp=sharing"
+    },
+    "Создать разрезы и сечения": {
+        "video": "https://vkvideo.ru/playlist/-222525145_4/video-222525145_456239128",
+        "pdf": "https://drive.google.com/file/d/1xi55LWtSPFSahXdpAghvDSy5JK0CPley/view?usp=sharing"
+    },
+    "Создание планов": {
+        "video": "https://vkvideo.ru/playlist/-222525145_4/video-222525145_456239097",
+        "pdf": "https://drive.google.com/file/d/1xi55LWtSPFSahXdpAghvDSy5JK0CPley/view?usp=sharing"
+    },
+    "Создание контура": {
+        "video": "https://vkvideo.ru/playlist/-222525145_4/video-222525145_456239055",
+        "pdf": "https://drive.google.com/file/d/1xi55LWtSPFSahXdpAghvDSy5JK0CPley/view?usp=sharing"
+    },
+    "Редактирование контура": {
+        "video": "https://vkvideo.ru/playlist/-222525145_4/video-222525145_456239056",
+        "pdf": "https://drive.google.com/file/d/1xi55LWtSPFSahXdpAghvDSy5JK0CPley/view?usp=sharing"
+    },
+    "Расчет продавливания": {
+        "video": "https://vkvideo.ru/playlist/-222525145_4/video-222525145_456239057",
+        "pdf": "https://drive.google.com/file/d/1xi55LWtSPFSahXdpAghvDSy5JK0CPley/view?usp=sharing"
+    },
+    "Создание каркасов": {
+        "video": "https://vkvideo.ru/playlist/-222525145_4/video-222525145_456239073",
+        "pdf": "https://drive.google.com/file/d/1xi55LWtSPFSahXdpAghvDSy5JK0CPley/view?usp=sharing"
+    },
+    "Создание видов каркасов": {
+        "video": "https://vkvideo.ru/playlist/-222525145_4/video-222525145_456239095",
+        "pdf": "https://drive.google.com/file/d/1xi55LWtSPFSahXdpAghvDSy5JK0CPley/view?usp=sharing"
+    },
+    # ОВ и ВК
+    "Муфты/гильзы": {
+        "video": "https://vkvideo.ru/playlist/-222525145_5/video-222525145_456239074",
+        "pdf": "https://drive.google.com/file/d/1xi55LWtSPFSahXdpAghvDSy5JK0CPley/view?usp=sharing"
+    },
+    "Аэродинамика": {
+        "video": "https://vkvideo.ru/playlist/-222525145_5/video-222525145_456239112",
+        "pdf": "https://drive.google.com/file/d/1xi55LWtSPFSahXdpAghvDSy5JK0CPley/view?usp=sharing"
+    },
+    "Создать виды систем": {
+        "video": "https://vkvideo.ru/playlist/-222525145_5/video-222525145_456239060",
+        "pdf": "https://drive.google.com/file/d/1xi55LWtSPFSahXdpAghvDSy5JK0CPley/view?usp=sharing"
+    },
+    "Спецификации систем": {
+        "video": "https://vkvideo.ru/playlist/-222525145_5/video-222525145_456239050",
+        "pdf": "https://drive.google.com/file/d/1xi55LWtSPFSahXdpAghvDSy5JK0CPley/view?usp=sharing"
+    },
+    "Высотные отметки": {
+        "video": "https://vkvideo.ru/playlist/-222525145_5/video-222525145_456239079",
+        "pdf": "https://drive.google.com/file/d/1xi55LWtSPFSahXdpAghvDSy5JK0CPley/view?usp=sharing"
+    },
+    "Толщина стенки": {
+        "video": "https://vkvideo.ru/playlist/-222525145_5/video-222525145_456239058",
+        "pdf": "https://drive.google.com/file/d/1xi55LWtSPFSahXdpAghvDSy5JK0CPley/view?usp=sharing"
+    },
+    "Диаметр изоляции": {
+        "video": "https://vkvideo.ru/playlist/-222525145_5/video-222525145_456239044",
+        "pdf": "https://drive.google.com/file/d/1xi55LWtSPFSahXdpAghvDSy5JK0CPley/view?usp=sharing"
+    },
+    "S изоляции": {
+        "video": "https://vkvideo.ru/playlist/-222525145_5/video-222525145_456239066",
+        "pdf": "https://drive.google.com/file/d/1xi55LWtSPFSahXdpAghvDSy5JK0CPley/view?usp=sharing"
+    },
+    # Боксы и отверстия
+    "Создание заданий": {
+        "video": "https://vkvideo.ru/playlist/-222525145_1/video-222525145_456239020",
+        "pdf": "https://drive.google.com/file/d/1xi55LWtSPFSahXdpAghvDSy5JK0CPley/view?usp=sharing"
+    },
+    "Объединение": {
+        "video": "https://vkvideo.ru/playlist/-222525145_1/video-222525145_456239078",
+        "pdf": "https://drive.google.com/file/d/1xi55LWtSPFSahXdpAghvDSy5JK0CPley/view?usp=sharing"
+    },
+    "Смещение": {
+        "video": "https://vkvideo.ru/playlist/-222525145_1/video-222525145_456239047",
+        "pdf": "https://drive.google.com/file/d/1xi55LWtSPFSahXdpAghvDSy5JK0CPley/view?usp=sharing"
+    },
+    "Обрезать": {
+        "video": "https://vkvideo.ru/playlist/-222525145_1/video-222525145_456239048",
+        "pdf": "https://drive.google.com/file/d/1xi55LWtSPFSahXdpAghvDSy5JK0CPley/view?usp=sharing"
+    },
+    "Нумерация": {
+        "video": "https://vkvideo.ru/playlist/-222525145_1/video-222525145_456239032",
+        "pdf": "https://drive.google.com/file/d/1xi55LWtSPFSahXdpAghvDSy5JK0CPley/view?usp=sharing"
+    },
+    "Отметка": {
+        "video": "https://vkvideo.ru/playlist/-222525145_1/video-222525145_456239028",
+        "pdf": "https://drive.google.com/file/d/1xi55LWtSPFSahXdpAghvDSy5JK0CPley/view?usp=sharing"
+    },
+    "Отверстия": {
+        "video": "https://vkvideo.ru/playlist/-222525145_1/video-222525145_456239022",
+        "pdf": "https://drive.google.com/file/d/1xi55LWtSPFSahXdpAghvDSy5JK0CPley/view?usp=sharing"
+    },
+    "Проверка пересечений": {
+        "video": "https://vkvideo.ru/playlist/-222525145_1/video-222525145_456239090",
+        "pdf": "https://drive.google.com/file/d/1xi55LWtSPFSahXdpAghvDSy5JK0CPley/view?usp=sharing"
+    },
+    # Renga
+    "Подсчет площадей": {
+        "video": "https://vkvideo.ru/video-222525145_456239109",
+        "pdf": "https://drive.google.com/file/d/1xi55LWtSPFSahXdpAghvDSy5JK0CPley/view?usp=sharing"
+    }
+}
